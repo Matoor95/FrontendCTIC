44e49b752cc82f4d227980d4b4bd4d857a8f429e
AminataDieye <n.aminatadieye@gmail.com>
2019-09-26 16:43:05 +0000

    img

--- src/app/component/nouvelle-edition/nouvelle-edition.component.ts
+++ src/app/component/nouvelle-edition/nouvelle-edition.component.ts
@@ -8,28 +8,38 @@ import { Router } from '@angular/router';
   styleUrls: ['./nouvelle-edition.component.css']
 })
 export class NouvelleEditionComponent implements OnInit {
-
+  file;
   constructor(private nouvelleEditionService: NouvelleEditionService, private router: Router) { }
 
   ngOnInit() {
   }
   sections = [];
+  changeFile(files){
+    //console.log(files);
+    this.file = files[0];
+  }
   ajouterSection(titre, description)
   {
+    const formData = new FormData();
+    formData.append('titre', titre);
+    formData.append('description', description);
+    formData.append('file', this.file);
     //this.sections = [titre, description]
-    this.sections.push({titre: titre, description: description});
-    //console.log("tableau", this.sections);
+    this.sections.push({titre: titre, description: description, image: this.file});
+
 
   }
+
+ 
   
-  ajouterEdition(date, numero, sections) {
+  ajouterEdition(date, numero) {
 
    // const edition= { date, numero, section }
    let edition = { numero: numero, date: date, sections: this.sections }
   
     this.nouvelleEditionService.ajouterEdition(edition).subscribe(
       (data) => {
-        console.log("tableauuu trouvé", edition.sections[0]);
+        console.log("tableau trouvé", edition.sections[0]);
 
         //this.router.navigateByUrl('/accueil-admin')
         alert(' Edition ajouté avec succes');
